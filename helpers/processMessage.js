5cb3c6e7c80702a2e5736838f31e8f92e51dc2f1
sreepriya <sreepriyav@live.com>
2017-11-09 17:50:43 -0500

    half working

---
+++ helpers/processMessage.js
@@ -0,0 +1,43 @@
+
+const API_AI_TOKEN = '4a2ba6369f024593b701a4b6117acbfe';
+const FACEBOOK_ACCESS_TOKEN = 'EAATlEqIf4W8BAMBp48PadCugUhISW9jZCpZBloTLwP34G7xCqLxGRZBEZA47XUpvpczC2mYKZB3IQdxrc8c55vgELGzjvbPGwZC1ZCHHtp174gV2FUJp0vZCOiszVxoCeCIkWKw8CFgeMlN8H0iZCNpC5DZBifwOJah7NPkh6ZB4stH5QZDZD';
+const CAT_IMAGE_URL = 'https://botcube.co/public/blog/apiai-tutorial-bot/hosico_cat.jpg';
+
+const request = require('request');
+
+const apiAiClient = require('apiai')(API_AI_TOKEN);
+
+const sendTextMessage = (senderId, text) => {
+    console.log(text);
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+        method: 'POST',
+        json: {
+            recipient: { id: senderId },
+            message: { text },
+        },
+    },(error, response) => {
+      if (error) {
+          console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error);
+      } });
+};
+
+module.exports = (event) => {
+    const senderId = event.sender.id;
+    const message = event.message.text;
+
+    const apiaiSession = apiAiClient.textRequest(message, {sessionId: 'CheerBot_co'});
+
+    apiaiSession.on('response', (response) => {
+       console.log("i m here")
+        const result = response.result.fulfillment.speech;
+         console.log(result);
+        sendTextMessage(senderId, result);
+    });
+
+    apiaiSession.on('error', error => console.log(error));
+    apiaiSession.end();
+};
